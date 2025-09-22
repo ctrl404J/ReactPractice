069caaea4ed22996d39722c13cee353577249712
Jo_ <2vwatt@gmail.com>
2025-09-23 02:37:19 +0900

    props를 사용한 컴포넌트로의 파라미터 전달

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import './App.css'
 function App() {
   let [postTitle, changeTitle] = useState(['Java에 관해서', 'Spring에 관해서', 'Python에 관해서']);
   postTitle.sort();
-  let [goodCount, changeGood] = useState([0,0,0]);
+  let [goodCount, changeGood] = useState(Array(postTitle.length).fill(0));
   let [modalState, setModal] = useState(false);
+  let [clickIndex, setClickIndex] = useState(0);
 
   return (
     <div className='App'>
@@ -21,6 +22,7 @@ function App() {
             <div className='list' key={i}>
                 <h4 onClick={()=>{
                   setModal(!modalState);
+                  setClickIndex(i);
                 }}>{postTitle[i]} <span onClick={()=>{
                   let copy = [...goodCount];
                   copy[i] = copy[i]+1;
@@ -32,18 +34,23 @@ function App() {
         })
       }
       {
-        modalState == true ? <Modal/> : null
+        modalState == true ? <Modal postTitle={postTitle} changeTitle={changeTitle} clickIndex={clickIndex}/> : null
       }
     </div>
   )
 }
 
-function Modal(){
+function Modal(props){
   return(
     <div className='modal'>
-      <h4>제목</h4>
+      <h4>{props.postTitle[props.clickIndex]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={()=>{
+        let copy = [...props.postTitle];
+        copy[0] = 'JS에 관해서';
+        props.changeTitle(copy);
+      }}>글수정</button>
     </div>
   )
 };
